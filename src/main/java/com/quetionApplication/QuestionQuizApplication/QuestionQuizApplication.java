e70e3088449a338185b8e77fb33aa8b17b219151
Sushma <sushmakumar19033@gmail.com>
2025-07-09 07:02:11 +0530

    Merge pull request #1 from SushmaKumar123/feature/test_case
    log message added in main class
    removed newline

--- src/main/java/com/quetionApplication/QuestionQuizApplication/QuestionQuizApplication.java
+++ src/main/java/com/quetionApplication/QuestionQuizApplication/QuestionQuizApplication.java
@@ -11,6 +11,7 @@ public class QuestionQuizApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionQuizApplication.class, args);
 		System.out.println("Question Quiz Application is running successfully");
+
 	}
 
 }
